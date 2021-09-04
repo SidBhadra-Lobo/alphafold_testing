b1ea9bcaedb935f2a468d0eb85a9ef52514eaf0f
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2021-09-04 00:30:09 -0400

    adding test sequences

--- alphafold_gpu.sh
+++ alphafold_gpu.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=gpu              # Partition (job queue)
 ##SBATCH --partition=p_ccib_1         # tpu_driver errors
 #SBATCH --requeue                    # Return job to the queue if preempted
-#SBATCH --job-name=alphafold         # Assign an short name to your job
+#SBATCH --job-name=af_6WZQ         # Assign an short name to your job
 #SBATCH --nodes=1                    # Number of nodes you require
 #SBATCH --ntasks=8                   # Total # of tasks across all nodes
 #SBATCH --cpus-per-task=1            # Cores per task (>1 if multithread tasks)
